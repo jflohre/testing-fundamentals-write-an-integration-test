b429b949a18bb53db76d68efd7abf695130b96db
Jacob Flohre <jacobflohre@gmail.com>
2019-05-14 22:34:53 -0400

    finished

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.1.6'
 gem 'rspec-rails', '~> 3.1'
-gem 'sqlite3'
+gem 'sqlite3', '~> 1.3.6'
 
 group :development do
   gem 'spring'
--- spec/features/user_creates_person_spec.rb
+++ spec/features/user_creates_person_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 feature "user creates person" do
   scenario "with valid data" do
-    pending "Use Capybara to create a new person"
+  	visit new_person_path
+  	fill_in 'First name', with: 'Jake'
+  	click_button 'Create Person'
+
+  	expect(page).to have_content('Jake')
   end
 end
